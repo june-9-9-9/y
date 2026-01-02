9c6b67c41e0e5f10314beca1a438086c77184976
june-9-9-9 <manenovincent78@gmail.com>
2026-01-03 00:15:28 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -102,7 +102,7 @@ const generateMenu = (pushname, currentMode, hostName, ping, uptimeFormatted, pr
 
     // Group Menu
     menu += `┏❐ \`GROUP MENU\` ❐\n`;
-    menu += `┃ ${prefix2}promote\n┃ ${prefix2}demote\n┃ ${prefix2}settings\n┃ ${prefix2}togroupstatus\n┃ ${prefix2}tosgroup\n┃ ${prefix2}welcome\n┃ ${prefix2}setgpp\n┃ ${prefix2}getgpp\n┃ ${prefix2}listadmin\n┃ ${prefix2}goodbye\n┃ ${prefix2}tagnoadmin\n┃ ${prefix2}tag\n┃ ${prefix2}antilink\n┃ ${prefix2}set welcome\n┃ ${prefix2}listadmin\n┃ ${prefix2}groupinfo\n┃ ${prefix2}admins\n┃ ${prefix2}warn\n┃ ${prefix2}revoke\n┃ ${prefix2}resetlink\n┃ ${prefix2}open\n┃ ${prefix2}close\n┃ ${prefix2}mention\n┃ ${prefix2}setgdesc\n┃ ${prefix2}leave\n┃ ${prefix2}left\n┃ ${prefix2}killall\n┃ ${prefix2}removeall\n┃ ${prefix2}pair┃ ${prefix2}link\n`;
+    menu += `┃ ${prefix2}promote\n┃ ${prefix2}demote\n┃ ${prefix2}settings\n┃ ${prefix2}togroupstatus\n┃ ${prefix2}tosgroup\n┃ ${prefix2}welcome\n┃ ${prefix2}setgpp\n┃ ${prefix2}getgpp\n┃ ${prefix2}listadmin\n┃ ${prefix2}goodbye\n┃ ${prefix2}tagnoadmin\n┃ ${prefix2}tag\n┃ ${prefix2}antilink\n┃ ${prefix2}set welcome\n┃ ${prefix2}listadmin\n┃ ${prefix2}groupinfo\n┃ ${prefix2}admins\n┃ ${prefix2}warn\n┃ ${prefix2}revoke\n┃ ${prefix2}resetlink\n┃ ${prefix2}open\n┃ ${prefix2}close\n┃ ${prefix2}mention\n┃ ${prefix2}setgdesc\n┃ ${prefix2}leave\n┃ ${prefix2}left\n┃ ${prefix2}killall\n┃ ${prefix2}removeall\n┃ ${prefix2}pair\n┃ ${prefix2}link\n`;
     menu += `┗❐\n\n`;
 
     // AI Menu
