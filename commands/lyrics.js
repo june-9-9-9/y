4d66872479ab6c6a50117b7b78f001dc30b428ec
june-9-9-9 <manenovincent78@gmail.com>
2026-01-05 08:34:19 +0300

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,30 +1,30 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 async function lyricsCommand(sock, chatId, songTitle, message) {
     if (!songTitle) {
         await sock.sendMessage(chatId, { 
             text: '🔍 Please enter the song name to get the lyrics! Usage: lyrics <song name>'
-        },{ quoted: message });
+        }, { quoted: message });
         return;
     }
 
-  try {
-    const res = await axios.get(`https://apiskeith.vercel.app/search/lyrics2?query=${encodeURIComponent(songTitle)}`);
-    const data = res.data;
+    try {
+        const res = await axios.get(`https://apiskeith.vercel.app/search/lyrics2?query=${encodeURIComponent(songTitle)}`);
+        const data = res.data;
 
-    if (!data.status || !data.result) {
-      return 
-      await sock.sendMessage(chatId, { text: "💢 Not found"}, [ quoted: message });
-    }
+        if (!data.status || !data.result) {
+            await sock.sendMessage(chatId, { text: "💢 Not found" }, { quoted: message });
+            return;
+        }
 
-    const caption = `🎶 ${data.result}`;
+        const caption = `🎶 ${data.result}`;
 
-    await sock.sendMessage(chatId, { text: caption }, { quoted: message });
-  } catch (error) {
+        await sock.sendMessage(chatId, { text: caption }, { quoted: message });
+    } catch (error) {
         console.error('Error in lyrics command:', error);
         await sock.sendMessage(chatId, { 
             text: `❌ An error occurred while fetching the lyrics for "${songTitle}".`
-        },{ quoted: message });
+        }, { quoted: message });
     }
 }
 
